dd7c40aa374497ba5091846cf39a141ec29d9997
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2021-11-30 15:32:46 +0100

    metà

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// NUMERI CASUALI
 const firstcomputerNumber = Math.floor(Math.random() * 100) + 1;
 alert(firstcomputerNumber);
 const secondcomputerNumber = Math.floor(Math.random() * 100) + 1;
@@ -8,3 +9,11 @@ const fourcomputerNumber = Math.floor(Math.random() * 100) + 1;
 alert(fourcomputerNumber);
 const fivecomputerNumber = Math.floor(Math.random() * 100) + 1;
 alert(fivecomputerNumber);
+// TIMER 30 SECONDI
+setTimeout(myFunction, 5000);
+// NUMERI CHE L' UTENTE DEVE INSERIRE
+function  myFunction() {
+    const userNumber = parseInt(prompt(`Inserisci i numeri visti in precendenza`));
+console.log(userNumber);
+}
+// VEDERE QUANTI NUMERI SONO STATI INDOVINATI
